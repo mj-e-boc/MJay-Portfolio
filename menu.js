7ba7413138976bce1c6a26943015cbffacbaa3d2
Ronald Mjonono <mjay@Ronalds-MacBook-Pro.local>
2023-04-18 21:32:08 +0200

    add mobile js

---
+++ menu.js
@@ -0,0 +1,11 @@
+
+const bar = document.querySelector('.bar');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  mobilemenu.classList.toggle('active');
+  bar.style.color = '#fff';
+});
+document.querySelectorAll('.menuitem').forEach((n) => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+  mobilemenu.classList.remove('active');
+}));
